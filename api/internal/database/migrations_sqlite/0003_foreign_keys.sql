891c0357d022bbe572dbedfba838e67b88f631d6
zzz <zhuzhenzhenit@163.com>
2026-07-20 12:31:40 +0800

    chore: add md-editor-v3 dependency and remove foreign key migrations

--- api/internal/database/migrations_sqlite/0003_foreign_keys.sql
+++
@@ -1,5 +0,0 @@
--- SQLite doesn't support easy ALTER TABLE ADD CONSTRAINT FOREIGN KEY.
--- Since the application layer already prevents taxonomy deletion via ErrTaxonomyInUse
--- and the Go code manually cleans up post_tags on post update, we can skip SQLite FKs here
--- to avoid full table rebuilds.
-SELECT 1;
